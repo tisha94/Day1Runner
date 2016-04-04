e7cba29348bfbab0baab266fae3e46abf3cd5de2
tisha94 <tisha_sutherland2001@aol.com>
2016-04-04 18:25:09 -0400

    Added files via upload

---
+++ Day1Runner.java
@@ -0,0 +1,16 @@
+package com.tiy.java.day1;  //<< package definition
+//base directory starts @com.tiy.java.day1
+//package ^^ means there should be a com. folder+ tiy + java + day1. 
+
+public class Day1Runner 
+{
+	public static void main(String[] args) 
+		{ 
+		System.out.println("Welcome Day 1 Runner :)"); //class(system). variable(out). method(println) on the variable. 
+
+//main method that outputs the following message^^
+		// to Compile: javac com/tiy/java/day1/*.java
+		// to Display-Run message in cmd:java com.tiy.java.day1.Day1Runner
+	}
+} 
+
